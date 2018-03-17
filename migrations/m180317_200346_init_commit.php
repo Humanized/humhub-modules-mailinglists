c19cde63972dd7c0a10dc482ca34a5e0318e0fdd
Jeffrey Geyssens <jeffrey@humanized.be>
2018-03-18 00:58:06 +0100

    Cleanup migration conventions

--- migrations/m180317_200346_init_commit.php
+++ migrations/m180317_200346_init_commit.php
@@ -5,6 +5,8 @@ use yii\db\Migration;
 class m180317_200346_init_commit extends Migration
 {
 
+    protected $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
     public function safeUp()
     {
         $this->createTable('mailing_list_membership', [
@@ -12,13 +14,13 @@ class m180317_200346_init_commit extends Migration
             'email' => $this->string(128)->notNull(),
             'is_member' => $this->boolean(),
             'token' => $this->string(128),
-        ]);
+                ], $this->tableOptions);
 
         $this->createTable('mailing_list_entry', [
             'id' => $this->primaryKey(),
             'template_instance_id' => $this->integer()->notNull(),
             'is_sent' => $this->boolean()->defaultValue(false)
-        ]);
+                ], $this->tableOptions);
 
         $this->addForeignKey(
                 'fk_mail_template_instance', 'mailing_list_entry', 'template_instance_id', 'template_instance', 'id', 'CASCADE'
