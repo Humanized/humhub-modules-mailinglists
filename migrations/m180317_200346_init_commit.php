719fdc732113ca923d3e608a5ec896b0597dd391
Jeffrey Geyssens <jeffrey@humanized.be>
2018-03-18 00:57:00 +0100

    Cleanup migration conventions

--- migrations/m180317_200346_init_commit.php
+++ migrations/m180317_200346_init_commit.php
@@ -4,28 +4,24 @@ use yii\db\Migration;
 
 class m180317_200346_init_commit extends Migration
 {
+
     public function safeUp()
     {
         $this->createTable('mailing_list_membership', [
             'id' => $this->primaryKey(),
-            'email' => $this->string(128),
-            'subscribed' => $this->boolean(),
-            'update_token' => $this->string(128),
+            'email' => $this->string(128)->notNull(),
+            'is_member' => $this->boolean(),
+            'token' => $this->string(128),
         ]);
 
         $this->createTable('mailing_list_entry', [
             'id' => $this->primaryKey(),
-            'template_instance_id' => $this->integer(),
-            'sent' => $this->boolean()->defaultValue(false)
+            'template_instance_id' => $this->integer()->notNull(),
+            'is_sent' => $this->boolean()->defaultValue(false)
         ]);
 
         $this->addForeignKey(
-            'fk-mail-template-instance',
-            'mailing_list_entry',
-            'template_instance_id',
-            'template_instance',
-            'id',
-            'CASCADE'
+                'fk_mail_template_instance', 'mailing_list_entry', 'template_instance_id', 'template_instance', 'id', 'CASCADE'
         );
     }
 
@@ -37,17 +33,17 @@ class m180317_200346_init_commit extends Migration
     }
 
     /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
+      // Use up()/down() to run migration code without a transaction.
+      public function up()
+      {
 
-    }
+      }
 
-    public function down()
-    {
-        echo "m180317_200346_init_commit cannot be reverted.\n";
+      public function down()
+      {
+      echo "m180317_200346_init_commit cannot be reverted.\n";
 
-        return false;
-    }
-    */
+      return false;
+      }
+     */
 }
