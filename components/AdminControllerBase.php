125962917cce96d654b436f480b252e0b8c9eb89
bkfox <thomas bkfox net>
2018-03-21 13:51:34 +0100

    includePage

--- components/AdminControllerBase.php
+++ components/AdminControllerBase.php
@@ -200,7 +200,6 @@ class AdminControllerBase extends Behavior
                 );
         }
 
-        return var_dump($model->includePage);
         // FIXME: wrong $model->includePage value (should be boolean, no str)
         $count = $entry->sendMails($members, $space, $model->includePage == "1");
         return $this->runEntries($count . ' mails have been successfully sent!');
